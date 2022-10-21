7c5171c72cf71c600bb5180fc9ee1f51535cd567
Jonatas Fabricio da Silva <joncleanmgr@gmail.com>
2022-10-21 09:40:25 -0300

    Initial Commit

---
+++ src/main/java/com/example/metaum/cursojavaspringboot/CursojavaspringbootApplication.java
@@ -0,0 +1,13 @@
+package com.example.metaum.cursojavaspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursojavaspringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursojavaspringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/metaum/cursojavaspringboot/CursojavaspringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.metaum.cursojavaspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursojavaspringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
