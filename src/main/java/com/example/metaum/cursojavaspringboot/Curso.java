36fb237c510ce526a8cf5b62d624b742cb852c83
Jonatas Fabricio da Silva <joncleanmgr@gmail.com>
2022-10-24 13:10:33 -0300

    .

---
+++ src/main/java/com/example/metaum/cursojavaspringboot/Curso.java
@@ -0,0 +1,18 @@
+package com.example.metaum.cursojavaspringboot;
+
+public class Curso {
+	
+	public String mostradados() {
+		
+		Aluno alunoA = new Aluno();
+		System.out.println(alunoA.getNOtaDoAluno(10,0));
+		
+		Aluno alunoB = new Aluno();
+		System.out.println(alunoB.getNOtaDoAluno(10,0));
+		
+		String valor = "Ola";
+		
+		return valor;
+	}
+
+}
