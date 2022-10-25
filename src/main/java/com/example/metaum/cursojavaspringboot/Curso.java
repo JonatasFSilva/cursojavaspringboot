4e900cccf3d78275bf65561c040690a23522bebd
Jonatas Fabricio da Silva <joncleanmgr@gmail.com>
2022-10-25 14:23:10 -0300

    .

--- src/main/java/com/example/metaum/cursojavaspringboot/Curso.java
+++ src/main/java/com/example/metaum/cursojavaspringboot/Curso.java
@@ -6,7 +6,7 @@ public class Curso {
 		
 		Aluno alunoA = new Aluno();
 		System.out.println(alunoA.getNOtaDoAluno(10,0));
-		
+		String test = "l";
 		Aluno alunoB = new Aluno();
 		System.out.println(alunoB.getNOtaDoAluno(10,0));
 		
