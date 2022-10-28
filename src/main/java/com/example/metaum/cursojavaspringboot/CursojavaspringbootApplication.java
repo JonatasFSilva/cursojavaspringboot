0b0a968d057e1626e601ca420be83827fa3f136a
Jonatas Fabricio da Silva <joncleanmgr@gmail.com>
2022-10-28 08:25:40 -0300

    Removendo classes nao utilizaveis

--- src/main/java/com/example/metaum/cursojavaspringboot/CursojavaspringbootApplication.java
+++ src/main/java/com/example/metaum/cursojavaspringboot/CursojavaspringbootApplication.java
@@ -1,30 +1,16 @@
 package com.example.metaum.cursojavaspringboot;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CursojavaspringbootApplication implements CommandLineRunner{
+public class CursojavaspringbootApplication {
 	
-	private static final Logger LOG = LoggerFactory.getLogger(CursojavaspringbootApplication.class);
-
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursojavaspringbootApplication.class, args);
 	}
 
-	@Override
-	public void run(String... args) throws Exception {
-		System.out.println("Iniciando o RUN...");
-		
-		Curso c = new Curso();
-		
-		c.mostradados();
-		
-		LOG.info("Finalizando o RUN");
-		
-	}
+
 
 }
