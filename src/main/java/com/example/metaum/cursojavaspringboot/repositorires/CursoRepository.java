ffbbc4f5c4b8245ef9fb5231ed2e0e19492326fc
Jonatas Fabricio da Silva <joncleanmgr@gmail.com>
2022-10-28 15:41:11 -0300

    criado interface CrudRepository

---
+++ src/main/java/com/example/metaum/cursojavaspringboot/repositorires/CursoRepository.java
@@ -0,0 +1,9 @@
+package com.example.metaum.cursojavaspringboot.repositorires;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.metaum.cursojavaspringboot.entities.Curso;
+
+public interface CursoRepository extends JpaRepository<Curso, Integer> {
+
+}
