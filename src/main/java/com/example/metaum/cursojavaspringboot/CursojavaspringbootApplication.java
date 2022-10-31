c87a00d0a21bf5df981a78956a555d48e59b08dd
Jonatas Fabricio da Silva <joncleanmgr@gmail.com>
2022-10-31 09:07:55 -0300

    creado insert(save) e update(save)

--- src/main/java/com/example/metaum/cursojavaspringboot/CursojavaspringbootApplication.java
+++ src/main/java/com/example/metaum/cursojavaspringboot/CursojavaspringbootApplication.java
@@ -1,5 +1,7 @@
 package com.example.metaum.cursojavaspringboot;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -24,8 +26,21 @@ public class CursojavaspringbootApplication implements CommandLineRunner {
 		Curso curso1 = new Curso("Graduacao em Ti");
 		Curso curso2 = new Curso("Graduacao em Economia");
 
+		//INSERT OR *UPDATE
 		repository.save(curso1);
 		repository.save(curso2);
+
+		List<Curso> listaDeCursos = repository.findAll();
+		
+		listaDeCursos.forEach(curso -> System.out.println(curso));
+		
+		System.out.println("Total de Cursos: " + repository.count());
+		
+		curso2.setNome("Graduacao em Administracao");
+		repository.save(curso2);
+		
+		listaDeCursos = repository.findAll();		
+		listaDeCursos.forEach(curso -> System.out.println(curso));
 	}
 
 }
