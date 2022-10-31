e95cf659c190e6311b270d2faba6820807b7fd01
Jonatas Fabricio da Silva <joncleanmgr@gmail.com>
2022-10-31 09:35:57 -0300

    criado o delete

--- src/main/java/com/example/metaum/cursojavaspringboot/CursojavaspringbootApplication.java
+++ src/main/java/com/example/metaum/cursojavaspringboot/CursojavaspringbootApplication.java
@@ -25,22 +25,35 @@ public class CursojavaspringbootApplication implements CommandLineRunner {
 
 		Curso curso1 = new Curso("Graduacao em Ti");
 		Curso curso2 = new Curso("Graduacao em Economia");
+		Curso curso3 = new Curso("Graduacao em Administracao");
+		Curso curso4 = new Curso("Graduacao em Educacao Fisica");
 
-		//INSERT OR *UPDATE
+		// INSERT OR *UPDATE
 		repository.save(curso1);
 		repository.save(curso2);
+		repository.save(curso3);
+		repository.save(curso4);
 
 		List<Curso> listaDeCursos = repository.findAll();
-		
+
 		listaDeCursos.forEach(curso -> System.out.println(curso));
+
+		System.out.println("Total de Cursos: " + repository.count());
+
+		/*
+		 * curso2.setNome("Graduacao em Administracao"); repository.save(curso2);
+		 * 
+		 * listaDeCursos = repository.findAll(); listaDeCursos.forEach(curso ->
+		 * System.out.println(curso));
+		 */
+		
+		repository.delete(curso1);
+		repository.deleteById(2);
+		
+		listaDeCursos = repository.findAll();
+		listaDeCursos.forEach(c -> System.out.println(c));
 		
 		System.out.println("Total de Cursos: " + repository.count());
-		
-		curso2.setNome("Graduacao em Administracao");
-		repository.save(curso2);
-		
-		listaDeCursos = repository.findAll();		
-		listaDeCursos.forEach(curso -> System.out.println(curso));
 	}
 
 }
--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,4 +1,4 @@
-INSERT INTO curso(nome_curso) VALUES ('Andre');
-INSERT INTO curso(nome_curso) VALUES ('Joana');
-INSERT INTO curso(nome_curso) VALUES ('Tobias');
-INSERT INTO curso(nome_curso) VALUES ('Mauricio');
+# INSERT INTO curso(nome_curso) VALUES ('Andre');
+# INSERT INTO curso(nome_curso) VALUES ('Joana');
+# INSERT INTO curso(nome_curso) VALUES ('Tobias');
+# INSERT INTO curso(nome_curso) VALUES ('Mauricio');
